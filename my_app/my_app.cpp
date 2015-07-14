ec5bdd4b0932ee9fb79bc689a29306e6ad7618d7
Jason Russell <jasrusable@gmail.com>
2015-07-14 15:25:20 +0200

    I can show an image, and my program has version numbers!

--- my_app/my_app.cpp
+++ my_app/my_app.cpp
@@ -1,5 +1,28 @@
+#include "my_app.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, world!\n";
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+	if (!image.data) {
+		cout << "Could not open or find the image." << std::endl;
+		return -1;
+	}
+	namedWindow("Display window", WINDOW_AUTOSIZE);
+	imshow("Display window", image);
+
+	waitKey(0);
+	return 0;
 }
