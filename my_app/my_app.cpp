2697ceba4c3be054ed8091b746dc3dc126512fca
Jason Russell <jasrusable@gmail.com>
2015-07-16 13:24:53 +0200

    function templates

--- my_app/my_app.cpp
+++ my_app/my_app.cpp
@@ -8,22 +8,12 @@
 using namespace cv;
 using namespace std;
 
+template <class T>
+T sum_test(T a, T b) {
+	return a + b;
+}
+
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
-		return -1;
-	}
-
-	Mat image;
-	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-
-	if (!image.data) {
-		cout << "Could not open or find the image." << std::endl;
-		return -1;
-	}
-	namedWindow("Display window", WINDOW_AUTOSIZE);
-	imshow("Display window", image);
-
-	waitKey(0);
-	return 0;
+	cout << sum_test(5, 6);
+	return 0;		
 }
