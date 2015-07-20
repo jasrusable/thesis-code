1085d55a524ceacfe692d199ab92daf17523fc50
Jason Russell <jasrusable@gmail.com>
2015-07-20 11:07:28 +0200

    dunno

--- my_app/my_app.cpp
+++ my_app/my_app.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
 #include "my_app.h"
 
-using namespace cv;
 using namespace std;
 
-template <class T>
-T sum_test(T a, T b) {
-	return a + b;
-}
 
 int main(int argc, char** argv) {
-	cout << sum_test(5, 6);
+	cout << "Hello World!" << endl;
 	return 0;		
 }
