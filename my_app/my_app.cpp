a598dd44ed59cfc27af5cb30185b0553a1de3a62
Jason Russell <jasrusable@gmail.com>
2015-07-22 13:06:30 +0200

    Load two images.

--- my_app/my_app.cpp
+++ my_app/my_app.cpp
@@ -1,11 +1,18 @@
+#include <stdio.h>
 #include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
 
-#include "my_app.h"
+using namespace cv;
 
-using namespace std;
+int main(int argc, char** argv){
+  
+  Mat img_object = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  Mat img_scene = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
 
-
-int main(int argc, char** argv) {
-	cout << "Hello World!" << endl;
-	return 0;		
+  if (!img_object.data || !img_scene.data) {
+    std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 }
