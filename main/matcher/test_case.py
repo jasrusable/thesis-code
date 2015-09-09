df99939a5853e546c4b312fa00166430caaa26e1
Jason Russell <jasrusable@gmail.com>
2015-09-09 19:15:12 +0200

    Fix looping over cases

--- main/matcher/test_case.py
+++ main/matcher/test_case.py
@@ -27,13 +27,13 @@ class TestCase(object):
                     setattr(new_preprocessor, parameter.name, i)
                     temp.append(new_preprocessor)
                 preprocessors_by_parameters.append(temp)
-            for c in itertools.product(*preprocessors_by_parameters):
-                self.tests.append(
-                    Test(
-                        test_image=copy.deepcopy(self.test_image),
-                        query_image=copy.deepcopy(self.query_image),
-                        detector=self.detector,
-                        matcher=self.matcher,
-                        test_preprocessors=c,
-                    )
+        for c in itertools.product(*preprocessors_by_parameters):
+            self.tests.append(
+                Test(
+                    test_image=copy.deepcopy(self.test_image),
+                    query_image=copy.deepcopy(self.query_image),
+                    detector=self.detector,
+                    matcher=self.matcher,
+                    test_preprocessors=c,
                 )
+            )
